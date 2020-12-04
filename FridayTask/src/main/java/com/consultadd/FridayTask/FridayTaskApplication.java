00ff6cf4488cd40bfe3978e9526c96fb9a5bdf9b
AdityaBConsultadd <aditya.b@consultadd.com>
2020-12-04 09:09:22 -0500

    friday task added

---
+++ FridayTask/src/main/java/com/consultadd/FridayTask/FridayTaskApplication.java
@@ -0,0 +1,13 @@
+package com.consultadd.FridayTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridayTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridayTaskApplication.class, args);
+	}
+
+}
---
+++ FridayTask/src/test/java/com/consultadd/FridayTask/FridayTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consultadd.FridayTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridayTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
